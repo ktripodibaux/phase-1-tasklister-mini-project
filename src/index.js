0158bdcf1f436e66e9217985b03e244bb95846ac
Kurt Baux <ktripodibaux@gmail.com>
2022-06-09 09:47:11 -0400

    working challenge problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = form.elements['new-task-description'].value;
+    createToDo(input);
+  })
+})
+
+function createToDo(todo) {
+  const task = todo;
+  const li= document.createElement('li');
+  const button = document.createElement('button');
+  const ul = document.getElementById('tasks');
+  li.textContent = task;
+  button.classList.add('delete');
+  button.textContent = 'X'
+  button.onclick = function () {this.parentNode.remove()};
+  li.append(button);
+  ul.append(li);
+}
